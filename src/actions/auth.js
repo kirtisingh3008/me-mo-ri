82ec975d1bee7a7372c185d01a788a7c53ee3bbb
kirtisingh3008 <kirtisingh3008@gmail.com>
2021-08-13 14:01:15 +0530

    client side

---
+++ src/actions/auth.js
@@ -0,0 +1,27 @@
+import { AUTH } from '../constants/actionTypes';
+import * as api from '../api/index.js';
+
+export const signin = (formData, history) => async(dispatch) => {
+    try{
+        // log in the uer
+        const { data } = await api.signIn(formData);
+        dispatch({type: AUTH, data});
+        history.push('/');
+    }
+    catch (error) {
+             console.log(error);
+    }
+
+}
+export const signup = (formData, history) => async(dispatch) => {
+    try{
+        // sign up the user
+        const { data } = await api.signUp(formData);
+        dispatch({type: AUTH, data});
+        history.push('/');
+    }
+    catch (error) {
+             console.log(error);
+    }
+
+}
